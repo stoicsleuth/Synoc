556674757667c1f203f36c9c1ac04da72c486021
stoicsleuth <tarashnkr321@gmail.com>
2018-01-11 03:36:08 +0530

    responsive tweaks

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ io.on('connection', (socket)=>{
     socket.on('createMessage',  (message, callback)=>{
         var user= users.getUser(socket.id);
         if(user && isString(message.text)){
-            io.to(user.room).emit('newMessage',generateMessage(user.room,message.text, user.color));
+            io.to(user.room).emit('newMessage',generateMessage(user.name,message.text, user.color));
         }
         
         callback();
