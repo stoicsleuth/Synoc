22641175a064c2a58e54f9caff4ffc9ae845ea85
stoicsleuth <tarashnkr321@gmail.com>
2018-01-13 23:10:13 +0530

    bug fixes

--- public/JS/room.js
+++ public/JS/room.js
@@ -8,6 +8,17 @@ socket.on('getRooms', (rooms)=>{
     rooms.forEach((room)=>{
         console.log(room);
         roomElem = jQuery('.rooms');
-        roomElem.append(jQuery('<p></p>').text(room));
+        roomElem.append(jQuery('<option></option>').text(room));
     });
 });
+
+
+
+$(".roomName").on("change keyup paste", function(){
+    tmpval = $(this).val();
+    if(tmpval !== '') {
+        jQuery('.roomParent').fadeOut('slow',()=>{});
+    } else{
+        jQuery('.roomParent').fadeIn('slow',()=>{});
+    }
+})
